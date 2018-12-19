62b306927834c58173cedfeb10df1cdfe9823aee
Gabriel Amancio <38165233+JecaTatu@users.noreply.github.com>
2018-12-19 19:20:53 -0300

    Organização do Canvas, adicionado função para arrastar div de grau

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,67 @@
+//Variáveis do canvas
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var point = {
+    x: 0,
+    y: 0,
+    radius: 3
+};
+var move = -1;
+var points = [];
+var degree = 2;
+var cs = [];
+
+//Função para arrasta a box do grau da curva
+dragElement(document.getElementById("grau"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // procura pela caixa do header para poder arrastar a partir daquele ponto
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // Caso não ache, arrasta de qualquer lugar de dentro da div
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // pega o ponto do mouse no inicio
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // chama a função onde quer que o mouse vá
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calcula a nova posição do mouse
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // seta o elemento na nova posição
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // para de mexer quando o botão é solto
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Função para adaptar o tamanho do canvas
 function resizeCanvas(width, height) {
     canvas.width = width;
     canvas.height = height;
 }
 
+// Função para tentar adaptar, ao máximo, a posição dos pontos no tamanho responsivo do canvas
 function resizeToFit() {
     var width = parseFloat(window.getComputedStyle(canvas).width);
     var height = parseFloat(window.getComputedStyle(canvas).height);
@@ -24,10 +83,20 @@ function isInCircle(click) {
     return -1;
 }
 
+// Funções onde se setam os pontos, baseado em onde o mouse se encontra
 function draw_point() {
+    var gradient = ctx.createLinearGradient(0, 1000, 2000, 0);
+gradient.addColorStop("0", "pink");
+gradient.addColorStop("0.5" ,"white");
+gradient.addColorStop("1.0", "red");
+
+// Fill with gradient
+ctx.strokeStyle = gradient;
+ctx.lineWidth = 5;
+//ctx.strokeRect(20, 20, 150, 100)
     ctx.beginPath();
-    ctx.strokeStyle = '#9D4B95';
-    ctx.lineWidth = '4';
+   // ctx.strokeStyle = '#9D4B95';
+    //ctx.lineWidth = '4';
     ctx.fillStyle = '#9D4B95';
     ctx.arc(point.x, point.y, point.radius, 0, 2*Math.PI);
     ctx.stroke();
@@ -42,6 +111,7 @@ function draw_points() {
     }
 }
 
+// Desenho do Canvas
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
@@ -52,20 +122,11 @@ function draw() {
     drawBezierCurve(findControlPoints(cs, points));
 }
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var point = {
-    x: 0,
-    y: 0,
-    radius: 3
-};
-var move = -1;
-var points = [];
-var degree = 2;
-var cs = [];
+
 
 resizeToFit();
 
+//Funções para se pegar as posições do mouse para se setar e arrastar os pontos
 canvas.addEventListener('mousedown', function(e) {
     move = isInCircle({
         x: e.offsetX,
@@ -97,6 +158,7 @@ canvas.addEventListener('mouseup', function(e) {
     draw();
 });
 
+// Adapta a curva da função de acordo com o grau setado
 var degreeSelector = document.getElementById("degreeSelector")
 degreeSelector.addEventListener("change", function() {
     degree = parseInt(degreeSelector.value)+1;
